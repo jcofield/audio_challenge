41cd37881a38cda2f47753138d7a105bdf331bd9
jcofield <joelcofield@gmail.com>
2019-05-07 10:28:40 -0700

    file to generate mel spec  and tempogram

---
+++ code/preprocess_data.py
@@ -0,0 +1,44 @@
+import librosa
+import pandas as pd
+import pickle
+import time
+
+
+def load_sample(fname, data_path='../input/train_curated/', SR=22050):
+    x, sr = librosa.load(data_path + '/' + fname)
+    if sr != SR:
+        raise Exception('Unexpected Sample Rate (not {}'.format(SR))
+    return x
+
+
+def get_logMel(y):
+    mel = librosa.feature.melspectrogram(y, power=2)
+    return (librosa.power_to_db(mel))
+
+
+def get_tempogram(y):
+    return (librosa.feature.tempogram(y))
+
+
+wavs = pd.read_csv("../input/train_curated.csv")
+
+mel = []
+spec = []
+tgram = []
+fnames = []
+tic = time.time()
+for i, row in wavs.iterrows():
+    y = load_sample(row.fname)
+    mel.append(get_logMel(y))
+    tgram.append(get_tempogram(y))
+    fnames.append(row.fname)
+    if i % 500 == 0:
+        print(i)
+        print("File {} processed after {}s".format(i, time.time() - tic))
+        with open('fname.pkl', 'wb') as fn:
+            pickle.dump(fnames, fn, pickle.HIGHEST_PROTOCOL)
+        with open('mel.pkl', 'wb') as fn:
+            pickle.dump(mel, fn, pickle.HIGHEST_PROTOCOL)
+        with open('tgram.pkl', 'wb') as fn:
+            pickle.dump(tgram, fn, pickle.HIGHEST_PROTOCOL)
+print(time.time() - tic)
